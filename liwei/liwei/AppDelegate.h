df3e6dd5a21cb543546a1c884b72de0eba6b4691
lanou3g <497689760@qq.com>
2015-07-13 15:06:40 +0800

    New object<15:06>

---
+++ liwei/liwei/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  liwei
+//
+//  Created by lanou3g on 15/7/13.
+//  Copyright (c) 2015年 Liwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ liwei/liwei/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  liwei
+//
+//  Created by lanou3g on 15/7/13.
+//  Copyright (c) 2015年 Liwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
